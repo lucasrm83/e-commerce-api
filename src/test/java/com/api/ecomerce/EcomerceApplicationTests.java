72ea79de62c94c1ec269443b7399831fb6e4bff4
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-04-26 13:19:21 -0300

    ecomerce springboot

---
+++ src/test/java/com/api/ecomerce/EcomerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ecomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
