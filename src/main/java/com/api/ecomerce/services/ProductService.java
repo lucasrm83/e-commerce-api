91934ef5a2447d15d612f7f88da3e54b9680d795
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-05-04 14:27:12 -0300

    ProductService

---
+++ src/main/java/com/api/ecomerce/services/ProductService.java
@@ -0,0 +1,28 @@
+package com.api.ecomerce.services;
+
+import com.api.ecomerce.entities.Product;
+import com.api.ecomerce.repositories.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    ProductRepository productRepository;
+
+    public List<Product> findAll(){
+        return productRepository.findAll();
+    }
+
+    public Product save(Product product){
+        return productRepository.save(product);
+    }
+
+    public void delete(Product product){
+        productRepository.delete(product);
+    }
+
+
+
+
+}
