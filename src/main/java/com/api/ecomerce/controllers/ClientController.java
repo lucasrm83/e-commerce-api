7319cbed8538dca61ca90c1765c929a7176e4870
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-05-12 15:57:46 -0300

    Clientcontroller basic crud finished

--- src/main/java/com/api/ecomerce/controllers/ClientController.java
+++ src/main/java/com/api/ecomerce/controllers/ClientController.java
@@ -1,8 +1,15 @@
 package com.api.ecomerce.controllers;
 
+import com.api.ecomerce.dto.ClientDto;
+import com.api.ecomerce.entities.Client;
 import com.api.ecomerce.services.ClientService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/clients")
@@ -13,4 +20,34 @@ public class ClientController {
     public ClientController(ClientService clientService) {
         this.clientService = clientService;
     }
+
+    @PostMapping
+    public ResponseEntity<Object> saveClient(Client client){
+        return ResponseEntity.status(HttpStatus.CREATED).body(clientService.save(client));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> findClient(@PathVariable(value = "id") UUID id){
+        Optional<Client> clientOptional =clientService.findById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(clientOptional.get());
+
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateClient(@PathVariable(value = "id")UUID id,@RequestBody Client client) {
+        Optional<Client> clientOptional = clientService.findById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(clientService.save(client));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteClient(@PathVariable(value = "id")UUID id) {
+        Optional<Client> clientOptional = clientService.findById(id);
+        clientService.delete(clientOptional.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Client Deleted! ");
+    }
+
+
+
+
+
+
 }
