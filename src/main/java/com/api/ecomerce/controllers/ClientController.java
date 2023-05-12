ce792bce9b525107dc33e9bdcc69b397efa195e5
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-05-11 23:50:47 -0300

    Client controller started

--- src/main/java/com/api/ecomerce/controllers/ClientController.java
+++ src/main/java/com/api/ecomerce/controllers/ClientController.java
@@ -1,7 +1,16 @@
 package com.api.ecomerce.controllers;
 
+import com.api.ecomerce.services.ClientService;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/clients")
 public class ClientController {
+
+    final ClientService clientService;
+
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
 }
